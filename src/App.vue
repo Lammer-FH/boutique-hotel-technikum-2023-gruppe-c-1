484793b87d550bfba5ae1a210f40c502bc7cb310
Jakob Iwancsich <jakobiwancsich@gmail.com>
2023-10-19 13:40:44 +0200

    router bearbeitet, About.vue/Impressum.vue/Navbar.vue/Footer.vue Komponente hinzugefügt, Bilder und Logo hinzugefügt, Daten bereinigt

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,34 @@
 <script>
+import router from './router/index.js'
 import Navbar from './components/navbar/Navbar.vue'
 import Footer from './components/footer/Footer.vue'
+import Impressum from './views/Impressum.vue'
+import About from './views/About.vue'
+
+
 export default {
   name: 'App',
-  components: {Navbar},
-  components: {Footer}
+  components: {
+    Navbar,
+    Footer,
+    router,
+    Impressum,
+    About
+  }
 }
 </script>
 
 <template>
   
-  <navbar class="navbar">
-    <ul class="navbar-content">
-        <li><router-link :to="{ name: 'Hotel'}" tag="li" class="list-group-item" active-class="active">Hotel</router-link></li>
-        <li><router-link :to="{ name: 'About Us'}" tag="li" class="list-group-item" active-class="active">About Us</router-link></li>
-        <li><router-link :to="{ name: 'Zimmer'}" tag="li" class="list-group-item" active-class="active">Zimmer</router-link></li>
-        <li><router-link :to="{ name: 'Buchen'}" tag="li" class="list-group-item" active-class="active">Buchen</router-link></li>
-        <li><router-link :to="{ name: 'Login'}" tag="li" class="list-group-item" active-class="active">Login</router-link></li>
-        <li><router-link :to="{ name: 'Registrieren'}" tag="li" class="list-group-item" active-class="active">Registrieren</router-link></li>
-    </ul>
-  </navbar>
+  <Navbar />
 
-  <footer class="footer">
-    <ul>
-      <li><router-link to="/impressum">Impressum</router-link></li>
-      <li><router-link to="/help">Help</router-link></li>
-      <li><router-link to="/account">My Account</router-link></li>
-      <li><router-link to="/about-us">About</router-link></li>
-    </ul>
-  </footer>
+  <About />
+
+  <Footer />
+  
 </template>
 
 <style>
-    .navbar {
-    width: 100%;
-    position: top static;
-    bottom: 0;
-    background-color: #f8f8f8;
-    display: flex;
-    justify-content: space-around;
-    padding: 10px 0;
-    box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1);
-  }
-  
-.navbar-content {
-  display: flex;
-  justify-content: center;
-}
-
-.navbar ul {
-  list-style: none;
-  display: flex;
-  gap: 20px;
-}
-
-.navbar router-link {
-  text-decoration: none;
-  color: inherit;
-}
-
-.navbar router-link:hover {
-  text-decoration: underline;
-}
-
+   
 </style>
-
-<style scoped>
-  .footer {
-    width: 100%;
-    position: bottom fixed;
-    bottom: 0;
-    background-color: #f8f8f8;
-    display: flex;
-    justify-content: space-around;
-    padding: 10px 0;
-    box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1);
-  }
-  
-.footer-content {
-  display: flex;
-  justify-content: center;
-}
-
-.footer ul {
-  list-style: none;
-  display: flex;
-  gap: 20px;
-}
-
-.footer router-link {
-  text-decoration: none;
-  color: inherit;
-}
-
-.footer router-link:hover {
-  text-decoration: underline;
-}
-
-  </style>
   
