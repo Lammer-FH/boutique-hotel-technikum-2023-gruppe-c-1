1d49d7b1f38b3a4b53c6ab70dfe731e9c828c3e2
Helle1 <helmuth.lammer@gmail.com>
2023-11-18 17:08:45 +0100

    removing deprecated bootstrap-vue-3 package and replaced it with bootstrap-vue-next, legacy installation steps, checking on Landingpage b-buttons was  positiv

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import './assets/css/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import BootstrapVueNext from 'bootstrap-vue-next'
 
-import BootstrapVue3 from 'bootstrap-vue-3'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/index.js'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(BootstrapVue3)
+app.use(BootstrapVueNext)
 
 app.mount('#app')
