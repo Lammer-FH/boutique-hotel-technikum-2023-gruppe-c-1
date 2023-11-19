6028e2e8953d0ce7a033eb8ec14ca3f19dc4b81e
Lukas Stuppacher <magicluks@live.at>
2023-11-19 16:16:01 +0100

    v.1.1.3-Bootstrap-Vue-Next eingebunden

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import './assets/css/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
-import BootstrapVue3 from 'bootstrap-vue-3'
+import BootstrapVueNext from 'bootstrap-vue-next'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/index.js'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(BootstrapVue3)
+app.use(BootstrapVueNext)
 
 app.mount('#app')
