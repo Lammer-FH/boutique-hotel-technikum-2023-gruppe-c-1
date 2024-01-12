57c14128c54b71f95c9407181915b31103086a1d
Lukas Stuppacher <magicluks@live.at>
2024-01-12 18:38:27 +0100

    u6-v.1.0.-login-and-registration-implemented-bugged

--- src/stores/accountCreationApiStore.js
+++ src/stores/accountCreationApiStore.js
@@ -5,9 +5,53 @@ const apiUrl = "https://boutique-hotel.helmuth-lammer.at/api/v1/";
 
 export const useAccountCreationApiStore = defineStore("accountCreationApi", {
     state: () => ({
-
+        token: '', 
+        loginHandlerApi: {
+            firstname: null,
+            lastname: null,
+            email: null,
+            birthdate: null,
+        }, 
     }),
+    getters:{
+        isLoggedIn: state => !!state.token,
+    },
     actions: {
-
+        setToken(token){
+            this.token = token;
+        },
+        async registerUser(firstname, lastname, email, username, password){
+            if(!this.isLoggedIn){
+                await axios.post(apiUrl + 'register', {
+                    firstname,
+                    lastname,
+                    email,
+                    username,
+                    password
+                }).then((response) => {
+                    const token = response.data;
+                    this.setToken(token);
+                    this.displayUserInfo();
+                }).catch((error) => {
+                    this.error = "An error has been detected";
+          	        console.log(error);
+                })
+            }else{
+                throw new Error('An Error has been detected');
+            }
+        },
+        async displayUserInfo(){
+            await axios.get(apiUrl + 'user', {
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                    Accept: 'application/json',
+                }
+            }).then((response) => {
+                this.loginHandlerApi = response.data;
+            }).catch((error) => {
+                this.error = "An error has been detected";
+                console.log(error);
+            })
+        },
     },
 });
