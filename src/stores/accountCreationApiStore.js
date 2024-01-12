80288ef79484279ff69bc7ad6c2b2c2bec69a3f5
Lukas Stuppacher <magicluks@live.at>
2024-01-12 16:49:55 +0100

    u6-v.0.1.-apiStore-files-created

---
+++ src/stores/accountCreationApiStore.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+const apiUrl = "https://boutique-hotel.helmuth-lammer.at/api/v1/";
+
+export const useAccountCreationApiStore = defineStore("accountCreationApi", {
+    state: () => ({
+
+    }),
+    actions: {
+
+    },
+});
